ab3ac7279b71f12c82e3845095ad74d2129e2dfd
Johannes <joheker@student.chalmers.se>
2021-12-06 15:31:32 +0100

    Day 6

--- src/Days/Day06.hs
+++ src/Days/Day06.hs
@@ -1,17 +1,31 @@
 module Days.Day06 where
 import Solution
+import Data.List (sum,sort,group)
+import Control.Arrow ((&&&))
 
-solA :: Show a => b -> Solution a
-solA xs = Pending
+-- First solved with huge list explicitly, waay too slow for part B
+solA :: Run [(Int,Int)] String
+solA = prep . sum . map snd . run 80
 
-solB :: Show a => b -> Solution a
-solB xs = Pending
+solB :: Run [(Int,Int)] String
+solB = prep . sum . map snd . run 256
 
+run 0 = id
+run x = run (x-1) . step
 
-parseA = id
-parseB = id
+step :: [(Int,Int)] -> [(Int,Int)]
+step xs = let (zs,vals) = takeWhile ((==0) . fst) &&& dropWhile ((==0) . fst) $ sort xs
+              births    = sum . map snd $ zs
+          in (6,births) : (8,births) : (map (dec . fst &&& snd) $ vals)
+  where
+    dec 0 = 6
+    dec x = x-1
 
+-- Parsing
+parseA = parse
+parseB = parse
 
+parse = map (head &&& length) . group . sort . map stoi . words . map (\x -> if x == ',' then ' ' else x)
 
 
 
