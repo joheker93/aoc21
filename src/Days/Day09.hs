dea4b1e17c42a533ed7871364afb3204254e97de
Johannes <joheker@student.chalmers.se>
2021-12-11 12:51:27 +0100

    Day 9

--- src/Days/Day09.hs
+++ src/Days/Day09.hs
@@ -1,18 +1,49 @@
+{-# LANGUAGE FlexibleContexts #-}
 module Days.Day09 where
 import Solution
 
-solA :: Show a => b -> Solution a
-solA xs = Pending
+import Data.List (intersperse,nub,sortBy)
+import Data.Ord (comparing)
+import Control.Arrow ((&&&))
 
-solB :: Show a => b -> Solution a
-solB xs = Pending
+solA :: Run Mat String
+solA  = prep . sum . map snd . map run . neighs
 
+run xs@(x:_) = if all (\p -> gx p < gy p) xs then (fst x , snd (fst x) + 1) else (fst x, 0)
+  where
+    gy (_,b) = snd b
+    gx (a,_) = snd a
 
-parseA = id
-parseB = id
+solB :: Run Mat String
+solB xs = let lpts      = map (fst . fst) . filter ((/=0) . snd) $ map run $ neighs xs
+              (ogi,ogv) = map (map fst) &&& map (map snd) $ xs
+              (sx,sy)   = length &&& length . head $ xs
+              basins    = map nub $ map (\(x,y) -> let v = ogv !! x !! y in exps sx sy ogv (x,y) (v,v) []) lpts
+              [f,s,t]   = take 3 $ sortBy (flip (comparing length)) basins
+          in prep (length f * length s * length t)
 
+exps :: Int -> Int -> [[Int]] -> (Int,Int) -> (Int,Int) ->  [(Int,Int)] -> [(Int,Int)]
+exps sx sy m t@(x,y) (v1,v2) a | prop = iter t
+                               | otherwise = a
+  where
+    prop    = inBound && not (elem t a) && incr && nv /= 9
+    incr    = nv >= v1 || nv == v2
+    iter v  = concat [exps sx sy m t (nv+1,nv) (v:a) | t <- [(x+1,y),(x-1,y),(x,y+1),(x,y-1)]]
+    nv      = (m !! x) !! y
+    inBound = x >= 0 && x < sx && y >= 0 && y < sy
 
+--Parsing
+type Mat = [[((Int,Int),Int)]]
+parseA = parse
+parseB = parse
 
-
-
-
+parse :: String -> Mat
+parse xs = let m = p xs
+               l = length $ head m
+               cs = chunks l (inds l)
+           in zipWith zip cs m
+  where
+    chunks _ [] = []
+    chunks n xs = take n xs : chunks n (drop n xs)
+    p = map (map stoi . words . intersperse ' ') . lines
+    inds n = [(a,b) | a <- [0..n-1], b <- [0..n-1]]
--- src/Solution.hs
+++ src/Solution.hs
@@ -6,6 +6,8 @@ import Data.Time (diffUTCTime, getCurrentTime)
 import Text.Printf (printf)
 import Control.DeepSeq
 import Control.Monad (ap)
+import Data.List(transpose,sortBy,groupBy)
+import Data.Ord (comparing)
 import qualified Data.Vector as V
 
 data Solution a = Pending | Sol a
@@ -14,7 +16,7 @@ type Input      = String
 type Run a b    = a -> Solution b
 
 
-
+-- Runs and prints a solution.
 solveDay :: Run String String -> Run String String -> String -> IO (String,String)
 solveDay sa sb p = do
   t1 <- getCurrentTime
@@ -22,14 +24,14 @@ solveDay sa sb p = do
   putStr $ "PART A : " ++ get r1
   t2 <- getCurrentTime
   let d2 = realToFrac $ diffUTCTime t2 t1 :: Double
-  putStrLn $ printf (", solved in (%.2f) seconds") d2
+  putStrLn $ printf (", solved in (%.10f) seconds") d2
 
   t1 <- getCurrentTime
   let r2 = sb p
   putStr $ "PART B : " ++ get r2
   t2 <- getCurrentTime
   let d2 = realToFrac $ diffUTCTime t2 t1 :: Double
-  putStrLn $ printf (", solved in (%.2f) seconds") d2
+  putStrLn $ printf (", solved in (%.10f) seconds") d2
 
   let r2 = sb p
   return (get r1,get r2)
@@ -39,7 +41,8 @@ instance Show a => Show (Solution a) where
   show Pending = "Pending"
   show (Sol x) = show x
 
-
+-- Auxi  --
+-- Util  --
 compose :: (Show b, Show c) => (a -> b) -> (a -> c) -> a -> (b,c)
 compose f g = ((,) <$> f <*> g)
 
@@ -62,3 +65,16 @@ btoi False = 0
 itob :: Int -> Bool
 itob 0 = False
 itob _ = True
+
+-- 2D array neighbour stuff
+rl (x:y:xs) = (x,y) : rl (y:xs)
+rl _        = []
+
+rightN m = m >>= rl
+leftN    = (map (\(a,b) -> (b,a)) .) rightN
+downN    = rightN . transpose
+upN      = (map (\(a,b) -> (b,a)) .) downN
+
+dirs = [rightN, leftN, downN, upN]
+neighs m = groupBy (\a b -> fst a == fst b) $ sortBy (comparing fst) ns
+  where ns = dirs >>= ($m)
