95c6b34f3d892cef61d21e703eb1dccec5d928c0
Johannes <joheker@student.chalmers.se>
2021-12-21 17:10:18 +0100

    Day 16 and 17

--- src/Days/Day15.hs
+++ src/Days/Day15.hs
@@ -1,18 +1,64 @@
 module Days.Day15 where
-import Solution
-
-solA :: Show a => b -> Solution a
-solA xs = Pending
-
-solB :: Show a => b -> Solution a
-solB xs = Pending
+import Solution hiding (neighs)
 
 
-parseA = id
-parseB = id
+import qualified Data.Map as M
+import Data.List (intersperse)
+import qualified Data.Graph.Inductive.Query.SP as SP
+import qualified Data.Graph.Inductive.Graph as G
+import qualified Data.Graph.Inductive.Tree as T
+import Data.Char(digitToInt)
+
+-- Running through an Shortest path library, not super fast
+
+solA :: Run [[Int]] String
+solA xs = let m = buildM M.empty 0 xs
+              gr = G.mkGraph (nodes m) (edges m (indexes 100 100) 100) :: T.Gr Int Int
+              spL = SP.spLength 0 9999 gr
+          in prep $ SP.spLength 0 9999 gr
 
 
 
+solB :: Run [[Int]] String
+solB xs = let m = buildM M.empty 0 (repl2 (repl xs))
+              gr = G.mkGraph (nodes m) (edges m (indexes 500 500) 500) :: T.Gr Int Int
+              sp = SP.sp 0  249999  gr
+          in prep $ SP.spLength 0 249999 gr
+  where
+    repl2 xs =  concat [xs, map (fmod 1) xs, map (fmod 2) xs, map (fmod 3) xs, map (fmod 4) xs]
 
+    repl [] = []
+    repl (xs:xss)  = concat [xs,fmod 1 xs, fmod 2 xs, fmod 3 xs, fmod 4 xs] : repl xss
+    fmod x = map ((\x -> if x > 9 then mod x 10 + 1 else x) . (+x))
 
+edges :: M.Map Int Int -> [(Int,Int)] -> Int -> [G.LEdge Int]
+edges m [] _     = []
+edges m (x:xs) s = edgeN x m s ++ edges m xs s
+
+edgeN (x,y) m s = let ids = [b * s + a | (a,b) <- [(x,y-1),(x+1,y),(x,y+1),(x-1,y)], a >= 0 && a < s, b >= 0 && b < s]
+                      id = y*s+x
+                      edges = foldl (\a n -> case M.lookup n m of
+                                        Nothing -> a
+                                        Just v  -> (id,n,v) : a) [] ids
+                  in edges
+
+nodes :: M.Map Int Int -> [G.LNode Int]
+nodes = map (\x -> (x,x)) . map fst . M.toList
+
+--Parsing
+parseA = parse
+parseB = parse
+
+parse = map (map stoi . words . intersperse ' ') . lines
+
+buildM :: M.Map Int Int -> Int -> [[Int]] -> M.Map Int Int
+buildM m i [] = m
+buildM m i (xs:xss) = let (m',i') = build m xs i
+                      in buildM m' i' xss
+
+build :: M.Map Int Int -> [Int] -> Int -> (M.Map Int Int,Int)
+build m [] i     = (m,i)
+build m (x:xs) i = build (M.insert i x m) xs (i+1)
+
+indexes h w = [(x,y) | x <- [0..w-1],y <- [0..h-1]]
 
--- src/Days/Day16.hs
+++ src/Days/Day16.hs
@@ -1,18 +1,88 @@
+{-# LANGUAGE LambdaCase #-}
 module Days.Day16 where
-import Solution
+import Solution hiding (Parser)
+import Numeric (readHex)
 
-solA :: Show a => b -> Solution a
-solA xs = Pending
+import Data.Char (digitToInt)
+import Data.List (foldl',sum)
+import Text.Parsec
+import Text.Parsec.String
+import Data.Maybe (listToMaybe)
+import Data.Maybe (mapMaybe)
+import Text.Printf
 
-solB :: Show a => b -> Solution a
-solB xs = Pending
+data Packet = Literal Version Type String | Operator Version Type [Packet] | End deriving Show
+type Version = String
+type Type    = String
+
+solA :: Run String String
+solA = prep . countVs . parse pPacket "" . pad
+  where
+    countVs = \case
+      Left x   -> error $ "RIP"
+      Right ps -> foldr (\x a -> toDec x + a) 0 $ flat ps
+    flat (Literal v _ _ )  = [v]
+    flat (Operator v _ ps) = v : foldl (\a x -> flat x ++ a) [] ps
 
 
-parseA = id
-parseB = id
+solB :: Run String String
+solB =  prep  . solve . parse pPacket "" . pad
+  where solve = \case
+          Left e -> error $ "Rip"
+          Right p -> packetVal p
 
+packetVal :: Packet -> Int
+packetVal (Literal _ _ v) = toDec v
+packetVal (Operator _ t ps) = case toDec t of
+  0 -> sum $ map packetVal ps
+  1 -> product $ map packetVal ps
+  2 -> minimum $ map packetVal ps
+  3 -> maximum $ map packetVal ps
+  5 -> compeq (>)  ps
+  6 -> compeq (<)  ps
+  7 -> compeq (==) ps
+  where
+    compeq f ps = let [l,r] = ps in if f (packetVal l) (packetVal r) then 1 else 0
 
+toDec :: String -> Int
+toDec = foldl' (\acc x -> acc * 2 + digitToInt x) 0
 
+-- Parsing
+parseA = parse'
+parseB = parse'
 
+parse' = concat . mapMaybe htob
 
+pad s | length s `mod` 4 == 0 = s
+      | otherwise = pad ('0':s)
+
+htob :: Char -> Maybe String
+htob c = case readHex [c] of
+    (x,_):_ -> Just $ printf "%04b" (x::Int)
+    _       -> Nothing
+
+values :: Parser String
+values = digit >>= \case
+  '0' -> count 4 digit
+  '1' -> count 4 digit >>= \ds -> values >>= return . ((++) <$> const ds <*> id)
+
+pPacket :: Parser Packet
+pPacket = do
+  (v,t) <- (,) <$> count 3 digit <*> count 3 digit
+  case t of
+    "100" -> values >>= return . Literal v t
+    x     -> digit >>= \case
+      '0' -> toDec <$> count 15 digit >>= iter >>= return . Operator v t
+      '1' -> toDec <$> count 11 digit >>= flip count pPacket >>= return . Operator v t
+
+  where
+    iter :: Int -> Parser [Packet]
+    iter l = do
+      g <- length <$> getInput
+      p <- pPacket
+      g2 <- length <$> getInput
+      if (g - g2) < l then do
+        ps <- iter (l - (g - g2))
+        return $ p : ps else do
+        return $ [p]
 
